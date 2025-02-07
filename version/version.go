581de65dec6303645dcabe49a4af02dc4a810548
wsodsong <wasuwee.sodsong@gmail.com>
2025-02-07 16:17:56 +0700

    update version

--- version/version.go
+++ version/version.go
@@ -7,8 +7,8 @@ import (
 const (
 	VersionMajor = 2     // Major version component of the current release
 	VersionMinor = 0     // Minor version component of the current release
-	VersionPatch = 2     // Patch version component of the current release
-	VersionMeta  = "" // Version metadata to append to the version string
+	VersionPatch = 3     // Patch version component of the current release
+	VersionMeta  = "dev" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
