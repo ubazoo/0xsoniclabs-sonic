079e51d6b391cb7503ca9c2fafffd988808d6c57
Facu <facumolina15@gmail.com>
2025-02-26 15:43:04 +0100

    Add block certificate json encode/decode (#68)
    * add committee certificate json struct and first tests
    
    * finish committee certificate encode/decode
    
    * move committee cert to ethapi. Add constructors/getters for committee and aggregated sign
    
    * add documentation and deep copy tests
    
    * add error checking in tests
    
    * remove unnecesary test
    
    * deleted faulty test. fix on documentation about shallow copies
    
    * blockCertJson initial commit
    
    * add conversion functions for block certificate json and tests

---
+++ ethapi/block_cert_json_test.go
@@ -0,0 +1,92 @@
+package ethapi
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/0xsoniclabs/sonic/scc"
+	"github.com/0xsoniclabs/sonic/scc/bls"
+	"github.com/0xsoniclabs/sonic/scc/cert"
+	"github.com/Fantom-foundation/lachesis-base/inter/idx"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBlockCertificateJson_ToBlockCertificate_ConvertsToHealthyCertificate(t *testing.T) {
+	require := require.New(t)
+	bitSet := cert.BitSet[scc.MemberId]{}
+	bitSet.Add(1)
+	newPrivateKey := bls.NewPrivateKey()
+	sig := newPrivateKey.GetProofOfPossession()
+
+	b := blockCertificateJson{
+		ChainId:   123,
+		Number:    456,
+		Hash:      common.Hash{0x1},
+		StateRoot: common.Hash{0x2},
+		Signers:   bitSet,
+		Signature: sig,
+	}
+
+	got := b.ToBlockCertificate()
+	aggregatedSignature := cert.NewAggregatedSignature[cert.BlockStatement](
+		b.Signers,
+		b.Signature,
+	)
+	want := cert.NewCertificateWithSignature(cert.NewBlockStatement(
+		b.ChainId,
+		idx.Block(b.Number),
+		b.Hash,
+		b.StateRoot),
+		aggregatedSignature)
+
+	require.Equal(want, got)
+}
+
+func TestBlockCertificateJson_CanBeJsonEncodedAndDecoded(t *testing.T) {
+	require := require.New(t)
+
+	// setup
+	private := bls.NewPrivateKey()
+	agg := cert.AggregatedSignature[cert.BlockStatement]{}
+	err := agg.Add(1, cert.Signature[cert.BlockStatement]{Signature: private.Sign([]byte{1})})
+	require.NoError(err)
+	c := cert.NewCertificateWithSignature(
+		cert.NewBlockStatement(123, 456, common.Hash{0x1}, common.Hash{0x2}),
+		agg,
+	)
+
+	// encode
+	certJson := blockCertificateToJson(c)
+	data, err := json.Marshal(certJson)
+	require.NoError(err)
+
+	// decode
+	var decoded blockCertificateJson
+	err = json.Unmarshal(data, &decoded)
+	require.NoError(err)
+
+	// check
+	require.Equal(certJson, decoded)
+	cert := decoded.ToBlockCertificate()
+	require.Equal(c, cert)
+}
+
+func TestBlockCertificateToJson(t *testing.T) {
+	require := require.New(t)
+	bitset := cert.BitSet[scc.MemberId]{}
+	bitset.Add(1)
+	sig := bls.NewPrivateKey().GetProofOfPossession()
+	cert := cert.NewCertificateWithSignature(
+		cert.NewBlockStatement(123, 456, common.Hash{0x1}, common.Hash{0x2}),
+		cert.NewAggregatedSignature[cert.BlockStatement](bitset, sig),
+	)
+
+	json := blockCertificateToJson(cert)
+	require.Equal(uint64(123), json.ChainId)
+	require.Equal(uint64(456), json.Number)
+	require.Equal(common.Hash{0x1}, json.Hash)
+	require.Equal(common.Hash{0x2}, json.StateRoot)
+	require.Equal(bitset, json.Signers)
+	require.Equal(sig, json.Signature)
+}
