55aac2ee224a12546986bdb0d0699cf33744515d
Facu <facumolina15@gmail.com>
2025-06-26 08:44:17 +0200

    Add license script (#335)
    * Add license script

---
+++ scripts/license/add_license_header.go
@@ -0,0 +1,230 @@
+// Copyright 2025 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+// add_license_header.go: Add or check license headers in project files
+// Usage: go run add_license_header.go [--check]
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+//go:embed license_header.txt
+var licenseHeader string
+
+func main() {
+	// process command line flags
+	checkOnly := flag.Bool("check", false,
+		"Check mode: only verify headers, do not modify files")
+
+	var targetDir string
+	flag.StringVar(&targetDir, "dir", "",
+		"Target directory to start processing files from. This flag is required to run.")
+
+	flag.Parse()
+
+	// get root dir from args
+	if len(targetDir) <= 0 {
+		log.Fatal("Please provide a directory to look for files, use -dir\n")
+	}
+	// Check if the directory exists
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		log.Fatalf("Invalid target directory: '%s'\n", targetDir)
+	}
+	fmt.Printf("Processing files in directory: %s\n", targetDir)
+
+	// This map defines the file patterns for files names or extensions, and
+	// their corresponding comment prefixes that will be added to the license header.
+	// Patterns that start with a dot (.) are treated as file extensions,
+	// while others are treated as specific file names.
+	patterns := map[string]string{
+		".go":         "//",
+		".yml":        "#",
+		"go.mod":      "//",
+		"Jenkinsfile": "//",
+		"BUILD":       "#",
+	}
+	// Process files with specified extensions
+	for ext, prefix := range patterns {
+		fmt.Printf("Processing files with extension %s using prefix '%s'\n", ext, prefix)
+		err := processFiles(targetDir, ext, prefix, licenseHeader, *checkOnly)
+		if err != nil {
+			log.Fatalf("Error processing files with extension %s: %v\n", ext, err)
+		}
+	}
+}
+
+// processFiles walks through the directory tree starting from dir,
+// finds files with the specified extension and processes them by adding or
+// checking the license header.
+//
+// checkOnly indicates whether to only check the headers without modifying files.
+// doubleHeader indicates whether to only check for double license headers.
+// if doubleHeader is true, checkOnly is ignored.
+func processFiles(dir, ext, prefix, license string, checkOnly bool) error {
+	licenseHeader := addPrefix(license, prefix)
+	var files []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return nil
+		}
+		// build files should not be checked
+		if shouldIgnore(path, []string{"/build/"}) {
+			return nil
+		}
+		if matchPattern(path, ext) {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to walk directory %s: %v", dir, err)
+	}
+	anyFails := false
+	for _, f := range files {
+		if err := processFile(f, licenseHeader, checkOnly); err != nil {
+			fmt.Println(err)
+			if !checkOnly {
+				// if check mode is not enabled, return the error
+				return err
+			}
+			// since check mode is enabled, record that there was a failure
+			anyFails = true
+		}
+		if checkOnly {
+			if err := checkDoubleHeader(f, prefix); err != nil {
+				fmt.Println(err)
+				anyFails = true
+			}
+		}
+	}
+	// return an error if there were any files that failed the check
+	if anyFails {
+		return fmt.Errorf("some files do not have the correct license header or have double headers")
+	}
+	return nil
+}
+
+// processFile checks if the file given has the correct license header and corrects it if requested.
+// If checkOnly is true, it only checks the header without modifying the file.
+// If the file has an old license header, it replaces it with the new one.
+// If the file does not have a license header, it adds the new one.
+// If the file has the correct license header, it does nothing.
+func processFile(file, licenseHeader string, checkOnly bool) error {
+	content, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %v", file, err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	licenseLines := strings.Split(strings.TrimSuffix(licenseHeader, "\n"), "\n")
+	needsUpdate := false
+
+	// check if the file has the first line of geth lincense header
+	if strings.Contains(lines[0], "The go-ethereum Authors") {
+		return nil
+	}
+
+	for i, l := range licenseLines {
+		if i >= len(lines) || strings.TrimSpace(lines[i]) != strings.TrimSpace(l) {
+			needsUpdate = true
+			break
+		}
+	}
+	if !needsUpdate {
+		return nil
+	}
+	if checkOnly {
+		return fmt.Errorf("missing or incorrect license header: %s", file)
+	}
+
+	// this means the file has an old license header, we need to replace it
+	if strings.Contains(lines[0], "Sonic Operations Ltd") {
+		// search for the first empty line after the old license header
+		for i, line := range lines {
+			if strings.TrimSpace(line) == "" {
+				// remove lines up to this point
+				content = []byte(strings.Join(lines[i+1:], "\n"))
+				break
+			}
+		}
+	}
+
+	// Add header
+	newContent := licenseHeader + "\n" + string(content)
+	return os.WriteFile(file, []byte(newContent), 0000)
+}
+
+func checkDoubleHeader(path, prefix string) error {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %v", path, err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	// if the first line does not contain "Copyright", we assume there is no license header
+	if !strings.Contains(lines[0], "Copyright") {
+		return nil
+	}
+	for i, line := range lines[1:] {
+		if strings.Contains(line, prefix+" Copyright") {
+			return fmt.Errorf("double license header found in %s at line %d: %s", path, i+1, strings.Split(line, "\n")[0])
+		}
+	}
+	return nil
+}
+
+// shouldIgnore checks if the file path should be ignored based on certain patterns.
+func shouldIgnore(path string, ignoredPaths []string) bool {
+	// the scripts ignores everything inside a build directory
+	for _, pathFragment := range ignoredPaths {
+		if strings.Contains(path, pathFragment) {
+			return true
+		}
+	}
+	return false
+}
+
+func matchPattern(path, pat string) bool {
+	if pat[0] == '.' {
+		return strings.HasSuffix(path, pat)
+	}
+	return filepath.Base(path) == pat
+}
+
+func addPrefix(license, prefix string) string {
+	var buf bytes.Buffer
+	s := bufio.NewScanner(strings.NewReader(license))
+	for s.Scan() {
+		line := s.Text()
+		if line == "" {
+			buf.WriteString(prefix + "\n")
+		} else {
+			buf.WriteString(prefix + " " + line + "\n")
+		}
+	}
+	return buf.String()
+}
---
+++ scripts/license/add_license_header_test.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Recognizes_GethLicense(t *testing.T) {
+	// make a temporary file
+	originalFileName := filepath.Join(t.TempDir(), "test_geth_license.go")
+
+	gethHeader := `Copyright 2014 The go-ethereum Authors
+				   This file is part of the go-ethereum library.
+				   
+				    The go-ethereum library is free software: you can redistribute it and/or modify
+				    it under the terms of the GNU Lesser General Public License as published by
+				    the Free Software Foundation, either version 3 of the License, or
+				    (at your option) any later version.
+				   
+				    The go-ethereum library is distributed in the hope that it will be useful,
+				    but WITHOUT ANY WARRANTY; without even the implied warranty of
+				    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+				    GNU Lesser General Public License for more details.
+				   
+				    You should have received a copy of the GNU Lesser General Public License
+				    along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.`
+
+	originalContent := []byte(gethHeader)
+	require.NoError(t, os.WriteFile(originalFileName, originalContent, 0644))
+	require.NoError(t, processFiles(originalFileName, ".go", "//", gethHeader, false))
+
+	contentAfter, err := os.ReadFile(originalFileName)
+	require.NoError(t, err)
+	require.Equal(t, originalContent, contentAfter)
+}
+
+func Test_Recognizes_CurrentSonicLicense(t *testing.T) {
+	// make a file in a temp folder
+	tmpFileName := filepath.Join(t.TempDir(), "test_license")
+	originalContent := []byte(addPrefix(licenseHeader, "//") + "\npackage main")
+	require.NoError(t, os.WriteFile(tmpFileName, originalContent, 0660))
+
+	require.NoError(t, processFiles(tmpFileName, ".go", "//", licenseHeader, false))
+
+	contentAfter, err := os.ReadFile(tmpFileName)
+	require.NoError(t, err)
+	require.Equal(t, originalContent, contentAfter)
+}
+
+func Test_Replaces_OldLicenseHeader(t *testing.T) {
+
+	tmpFileName := filepath.Join(t.TempDir(), "test_license.go")
+	// write a sample license header to the file
+	oldLicense := `Copyright 2024 Sonic Operations Ltd
+				   This file is part of some old version
+				   of the Sonic Client`
+	require.NoError(t,
+		os.WriteFile(
+			tmpFileName,
+			[]byte(addPrefix(oldLicense, "//")+"\npackage main\n"),
+			0660))
+
+	require.NoError(t, processFiles(tmpFileName, ".go", "//", licenseHeader, false))
+
+	contentAfter, err := os.ReadFile(tmpFileName)
+	require.NoError(t, err)
+	require.Contains(t, string(contentAfter), addPrefix(licenseHeader, "//"))
+
+	require.NotContains(t, string(contentAfter), oldLicense)
+}
+
+func Test_Adds_LicenseHeader(t *testing.T) {
+	tmpFileName := filepath.Join(t.TempDir(), "test_license.go")
+	require.NoError(t, os.WriteFile(tmpFileName, []byte("package main\n\nfunc main() {}\n"), 0660))
+
+	require.NoError(t, processFiles(tmpFileName, ".go", "//", licenseHeader, false))
+
+	contentAfter, err := os.ReadFile(tmpFileName)
+	require.NoError(t, err)
+	extendLicenseHeader := addPrefix(licenseHeader, "//")
+	require.Contains(t, string(contentAfter), extendLicenseHeader)
+}
+
+func Test_Detects_DoubleHeader(t *testing.T) {
+	tmpFileName := filepath.Join(t.TempDir(), "test_license.go")
+	doubleHeaderString := addPrefix(licenseHeader, "//") +
+		addPrefix(licenseHeader, "//") +
+		"\npackage main"
+	require.NoError(t, os.WriteFile(tmpFileName, []byte(doubleHeaderString), 0660))
+
+	// Check for double license headers
+	require.Error(t, checkDoubleHeader(tmpFileName, "//"))
+}
+
+func Test_OnlyOneEmptyLineAfterHeader(t *testing.T) {
+	tmpFileName := filepath.Join(t.TempDir(), "test_license.go")
+
+	fileWithHeader := "// This is some documentation\npackage main\nfunc main() {}\n"
+	require.NoError(t, os.WriteFile(tmpFileName, []byte(fileWithHeader), 0660))
+
+	require.NoError(t, processFiles(tmpFileName, ".go", "//", licenseHeader, false))
+
+	contentAfter, err := os.ReadFile(tmpFileName)
+	require.NoError(t, err)
+
+	alreadyFoundEmptyLine := false
+	for i, line := range strings.Split(string(contentAfter), "\n") {
+		if len(line) == 0 {
+			if !alreadyFoundEmptyLine {
+				alreadyFoundEmptyLine = true
+				continue // first empty line after the header
+			}
+			// if we found a second empty line, fail the test
+			require.Fail(t, "There should be only one empty line after the license header", i)
+		}
+		// there is a non-empty line after the first empty line
+		if alreadyFoundEmptyLine {
+			break // all is good
+		}
+	}
+}
